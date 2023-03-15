b04fcdf6cc479f3c6a68623dfe21cd33f13235a0
Fernand0Leal <127244303+Fernand0Leal@users.noreply.github.com>
2023-03-15 19:46:19 +0000

    Camera Improvement

---
+++ Assets/Scripts/CRot.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CRot : MonoBehaviour
+{
+    public GameObject rotatedObject;
+    public float rotationSpeed;
+    bool rotateRight = false;
+    bool rotateLeft = false;
+    public Button cameraB;
+    public Button cameraA;
+
+    void Start () {
+        Button btn = cameraB.GetComponent<Button>();
+        Button btn1 = cameraA.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        btn1.onClick.AddListener(TaskOnClick1);
+
+    
+    }
+    void TaskOnClick () {
+       RotateCarRight(); 
+    }
+    void TaskOnClick1(){
+        RotateCarLeft();
+    }
+ 
+    public void RotateCarRight(){
+        rotateRight = !rotateRight;
+    }
+
+    public void RotateCarLeft(){
+        rotateLeft = !rotateLeft;
+    }
+
+    public void Update(){
+        if (rotateRight){
+            rotationSpeed = 20;
+            transform.Rotate(0,rotationSpeed*Time.deltaTime,0);
+        }
+
+        if (rotateLeft){
+           rotationSpeed = -20;
+           transform.Rotate(0,rotationSpeed*Time.deltaTime,0);
+        }
+    }
+    }
+
--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -5,20 +5,21 @@ using UnityEngine.UI;
 
 public class RotateCamera : MonoBehaviour
 {
-    public GameObject target;//the target object
-    private float speedMod = 2000.0f;//a speed modifier
-    private Vector3 point;//the coord to the point where the camera looks at
+   // public GameObject target;//the target object
+    public float speedMod;//a speed modifier
+    //private Vector3 point;//the coord to the point where the camera looks at
     public Button cameraB;
  
     void Start () {//Set up things on the start method
         Button btn = cameraB.GetComponent<Button>();
         btn.onClick.AddListener(TaskOnClick);
 
-        point = target.transform.position;//get target's coords
-        transform.LookAt(point);//makes the camera look to it
+       // point = target.transform.position;//get target's coords
+       // transform.LookAt(point);//makes the camera look to it
     }
+
  
     void TaskOnClick () {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
-        transform.RotateAround (target.transform.position, Vector3.up, speedMod * Time.deltaTime);  
+        transform.Rotate (0, speedMod * Time.deltaTime,0);  
     }
 }
