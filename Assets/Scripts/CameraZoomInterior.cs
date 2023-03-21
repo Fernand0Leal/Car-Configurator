b8bdef5d09088e198a9d757480f6e694371ca81f
Fernand0Leal <127244303+Fernand0Leal@users.noreply.github.com>
2023-03-21 03:29:07 +0000

    DoorAnimation

---
+++ Assets/Scripts/CameraZoomInterior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CameraZoomInterior : MonoBehaviour
+{
+    Animator dAnimator;
+    public Button interiorB;
+    public bool door;
+    
+
+    
+    
+    void Start (){
+
+        
+
+        Button btn = interiorB.GetComponent<Button>();
+
+        btn.onClick.AddListener(TaskOnClick);
+        dAnimator = GetComponent<Animator>();
+        
+
+
+
+
+    }
+
+    public void TaskOnClick(){
+
+        door = !door;
+        dAnimator.SetBool ("ButtonPress", door);
+        
+    }
+ }
+
